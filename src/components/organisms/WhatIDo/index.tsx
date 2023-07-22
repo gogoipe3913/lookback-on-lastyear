1e2144f4da4cbc70a9657a4ec4fd00e7bfd090aa
Taiki Kishiyama <taiki.kishiyama0913@gmail.com>
2023-07-23 03:39:40 +0900

    レスポンシブ対応

--- src/components/organisms/WhatIDo/index.tsx
+++ src/components/organisms/WhatIDo/index.tsx
@@ -26,6 +26,9 @@ const images = [
   {
     src: "../../../../public/images/whatIDo/risograph.png",
   },
+  {
+    src: "../../../../public/images/whatIDo/risograph.png",
+  },
 ];
 
 type WhatIDoProps = {
@@ -33,21 +36,13 @@ type WhatIDoProps = {
 };
 
 const WhatIDo: React.FC<WhatIDoProps> = ({ className = "" }) => {
-  const AUTO_SLIDE_INTERVAL_SECOND = 8000; // 8000msで自動スライド
+  const AUTO_SLIDE_INTERVAL = 8000; // 8000msで自動スライド
   const SET_INTERVAL_SECOND = 10; // 10ms毎にプログレスバーの幅を再計算
-  const PROGRESS_MAX_WIDTH = 100; // 100pxが最大幅
 
-  // const [isMouseOverPrev, setIsMouseOverPrev] = useState(false);
-  // const [isMouseOverNext, setIsMouseOverNext] = useState(false);
-  // const prevButtonRef = useRef<HTMLDivElement | null>(null);
-  // const nextButtonRef = useRef<HTMLDivElement | null>(null);
+  const [currentSlideNumber, setCurrentSlideNumber] = useState(0);
+  const [progressBarWidth, setProgressBarWidth] = useState(0);
+  const sliderRef = useRef<Slider | null>(null);
 
-  const [intervalId, setIntervalId] = useState(0);
-
-  const [currentSlideNumber, setCurrentSlideNumber] = useState(1); // 枚数中のcurrent
-  const [currentSlideSerialNumber, setCurrentSlideSerialNumber] = useState(1); // 通し番号中のcurrent
-  const [progressBarCurrentWidth, setProgressBarCurrentWidth] = useState(0);
-  const [slider, setSlider] = useState<Slider | null>(null);
   const settings: Settings = {
     initialSlide: 0,
     dots: false,
@@ -56,41 +51,47 @@ const WhatIDo: React.FC<WhatIDoProps> = ({ className = "" }) => {
     speed: 500,
     waitForAnimate: true,
     slidesToShow: 3,
+    centerMode: true,
+    centerPadding: "0px", // 追加：センターモード時のスライド間の余白を0に設定
+    beforeChange: (current, next) => {
+      setCurrentSlideNumber(next);
+      setProgressBarWidth(0); // スライド切り替え時にプログレスバーをリセット
+    },
   };
 
   useEffect(() => {
-    let width = 0;
-    // 前にuseEffectしたタイミングのintervalは停止しておく
-    clearInterval(intervalId);
+    let intervalId: number | null = null;
+    let progressBarInterval: number | null = null;
 
-    setIntervalId(
-      setInterval(() => {
-        if (width >= AUTO_SLIDE_INTERVAL_SECOND / SET_INTERVAL_SECOND) {
-          setProgressBarCurrentWidth(0);
-          width = 0;
-          setCurrentSlideSerialNumber(currentSlideSerialNumber + 1);
-          if (currentSlideNumber < 6) {
-            setCurrentSlideNumber(currentSlideNumber + 1);
-          } else {
-            setCurrentSlideNumber(
-              currentSlideNumber - 5 * Math.trunc(currentSlideNumber / 6)
-            );
-          }
-        } else {
-          width++;
-          setProgressBarCurrentWidth(
-            width /
-              (AUTO_SLIDE_INTERVAL_SECOND /
-                SET_INTERVAL_SECOND /
-                PROGRESS_MAX_WIDTH)
-          );
-        }
-      }, SET_INTERVAL_SECOND)
-    );
+    const startAutoSlide = () => {
+      intervalId = window.setInterval(() => {
+        const nextSlide = (currentSlideNumber + 1) % images.length;
+        sliderRef.current?.slickGoTo(nextSlide);
+      }, AUTO_SLIDE_INTERVAL);
 
-    if (slider) {
-      slider.slickGoTo(currentSlideSerialNumber - 1);
-    }
+      // プログレスバーのアニメーションをセット
+      progressBarInterval = window.setInterval(() => {
+        setProgressBarWidth((prevWidth) => {
+          const remainingWidth = AUTO_SLIDE_INTERVAL - SET_INTERVAL_SECOND;
+          return Math.min(prevWidth + SET_INTERVAL_SECOND, remainingWidth);
+        });
+      }, SET_INTERVAL_SECOND);
+    };
+
+    const stopAutoSlide = () => {
+      if (intervalId) {
+        clearInterval(intervalId);
+      }
+      if (progressBarInterval) {
+        clearInterval(progressBarInterval);
+      }
+    };
+
+    startAutoSlide();
+
+    return () => {
+      stopAutoSlide();
+    };
   }, [currentSlideNumber]);
 
   return (
@@ -102,81 +103,13 @@ const WhatIDo: React.FC<WhatIDoProps> = ({ className = "" }) => {
           <span className={style.WhatIDo__titleRow}>What I do?</span>
         </p>
         <div className={style.WhatIDo__sliderWrapper}>
-          <Slider
-            ref={(_slider) => {
-              setSlider(_slider);
-            }}
-            {...settings}
-          >
+          <Slider ref={sliderRef} {...settings}>
             {images.map((img, index) => (
               <div
                 key={index}
-                // onClick={() => {
-                //   if (isMouseOverPrev) {
-                //     clearInterval(intervalId);
-                //     setCurrentSlideSerialNumber(currentSlideSerialNumber - 1);
-                //     if (currentSlideNumber > 1) {
-                //       setCurrentSlideNumber(currentSlideNumber - 1);
-                //     } else {
-                //       setCurrentSlideNumber(currentSlideNumber + 5);
-                //     }
-                //   }
-
-                //   if (isMouseOverNext) {
-                //     clearInterval(intervalId);
-                //     setCurrentSlideSerialNumber(currentSlideSerialNumber + 1);
-                //     if (currentSlideNumber < 6) {
-                //       setCurrentSlideNumber(currentSlideNumber + 1);
-                //     } else {
-                //       setCurrentSlideNumber(currentSlideNumber - 5);
-                //     }
-                //   }
-                // }}
-                // onMouseEnter={() => {
-                //   if (currentSlideNumber - 1 === index && !isMouseOverPrev) {
-                //     setIsMouseOverPrev(true);
-                //   }
-                //   if (currentSlideNumber + 1 === index && !isMouseOverNext) {
-                //     setIsMouseOverNext(true);
-                //   }
-                //   document.body.style.cursor = "none";
-                // }}
-                // onMouseMove={(event) => {
-                //   const top = event.clientY;
-                //   const left = event.clientX;
-
-                //   if (
-                //     isMouseOverPrev &&
-                //     prevButtonRef &&
-                //     prevButtonRef.current
-                //   ) {
-                //     prevButtonRef.current.style.top = `${top}px`;
-                //     prevButtonRef.current.style.left = `${left}px`;
-                //   }
-
-                //   if (
-                //     isMouseOverNext &&
-                //     nextButtonRef &&
-                //     nextButtonRef.current
-                //   ) {
-                //     nextButtonRef.current.style.top = `${top}px`;
-                //     nextButtonRef.current.style.left = `${left}px`;
-                //   }
-                // }}
-                // onMouseLeave={() => {
-                //   if (currentSlideNumber - 1 === index && isMouseOverPrev) {
-                //     setIsMouseOverPrev(false);
-                //   }
-                //   if (currentSlideNumber + 1 === index && isMouseOverNext) {
-                //     setIsMouseOverNext(false);
-                //   }
-
-                //   document.body.style.cursor = "default";
-                // }}
                 className={classNames(
                   style.WhatIDo__sliderContent,
-                  currentSlideNumber === index ||
-                    (currentSlideNumber === 6 && index === 0)
+                  currentSlideNumber === index
                     ? style["WhatIDo__sliderContent--current"]
                     : ""
                 )}
@@ -185,45 +118,29 @@ const WhatIDo: React.FC<WhatIDoProps> = ({ className = "" }) => {
               </div>
             ))}
           </Slider>
-          {/* {isMouseOverPrev ? (
-            // HTMLButtonElementだと表示がうまくいかないのでdivで処理
-            <div
-              ref={prevButtonRef}
-              className={style.WhatIDo__sliderContentButton}
-            >
-              <span>Prev</span>
-            </div>
-          ) : null}
-          {isMouseOverNext ? (
-            // HTMLButtonElementだと表示がうまくいかないのでdivで処理
-            <div
-              ref={nextButtonRef}
-              className={style.WhatIDo__sliderContentButton}
-            >
-              <span>Next</span>
-            </div>
-          ) : null} */}
         </div>
         <p className={style.WhatIDo__currentSlideIndicator}>
           <span className={style.WhatIDo__progressBars}>
             <span className={style.WhatIDo__progressBarBase} />
             <span
               className={style.WhatIDo__progressBarCurrent}
-              style={{ width: `${progressBarCurrentWidth}px` }}
+              style={{
+                width: `${(progressBarWidth / AUTO_SLIDE_INTERVAL) * 100}%`,
+              }}
             />
           </span>
-          <span
-            className={style.WhatIDo__numbers}
-          >{`0${currentSlideNumber} / 06`}</span>
+          <span className={style.WhatIDo__numbers}>{`0${
+            currentSlideNumber + 1
+          } / 06`}</span>
         </p>
       </FadeInContainer>
       <FadeInContainer>
         <div className={style.WhatIDo__slideText}>
           <p className={style.WhatIDo__slideTitle}>
-            {slideContents[currentSlideNumber - 1].title}
+            {slideContents[currentSlideNumber].title}
           </p>
           <p className={style.WhatIDo__slideContent}>
-            {slideContents[currentSlideNumber - 1].text}
+            {slideContents[currentSlideNumber].text}
           </p>
         </div>
       </FadeInContainer>
