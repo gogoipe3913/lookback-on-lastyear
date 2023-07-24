f588c2e65c217d1db2f059a18b54e642ed85f5ea
Taiki Kishiyama <taiki.kishiyama0913@gmail.com>
2023-07-25 02:31:42 +0900

    パスのビルド設定を修正

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./", //相対パスでビルドする
+  root: "./src", //開発ディレクトリ設定
   plugins: [react()],
 });
