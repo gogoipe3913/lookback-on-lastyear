a89d3f6e24594e5e26b7e746fd07dcfdb45c2032
Taiki Kishiyama <taiki.kishiyama0913@gmail.com>
2024-08-18 14:16:12 +0900

    Loadingの修正

--- src/components/organisms/Top/index.tsx
+++ src/components/organisms/Top/index.tsx
@@ -12,43 +12,45 @@ const Top: React.FC = () => {
   };
 
   return (
-    <div id="Top" className={style.Top}>
+    <>
       {isLoading && (
         <div className={style.Loading}>
           <p className={style.Loading__title}>LOOKBACK ON LASTYEAR</p>
         </div>
       )}
-      <div
-        className={classNames(
-          style.Top__videoWrapper,
-          style["Top__videoWrapper--displayed"]
-        )}
-      >
-        <p className={style.Top__titleBox}>
-          <span className={style.Top__title}>LOOKBACK ON LASTYEAR</span>
-        </p>
-        <div className={style.Top__videoFrame}>
-          <video
-            playsInline
-            muted={true}
-            autoPlay={true}
-            loop={true}
-            src="/video/topVisualVideo.mp4"
-            onLoadedData={handleVideoLoad}
-            className={style.Top__video}
-          />
-          <div className={style.Top__titleBoxOnImage}>
-            <span className={style.Top__titleOnImage}>
-              LOOKBACK ON LASTYEAR
-            </span>
-            <span className={style.Top__titleOnImageSubTitle}>
-              + hello 2023, what i do?
-            </span>
+      <div id="Top" className={style.Top}>
+        <div
+          className={classNames(
+            style.Top__videoWrapper,
+            style["Top__videoWrapper--displayed"]
+          )}
+        >
+          <p className={style.Top__titleBox}>
+            <span className={style.Top__title}>LOOKBACK ON LASTYEAR</span>
+          </p>
+          <div className={style.Top__videoFrame}>
+            <video
+              playsInline
+              muted={true}
+              autoPlay={true}
+              loop={true}
+              src="/video/topVisualVideo.mp4"
+              onLoadedData={handleVideoLoad}
+              className={style.Top__video}
+            />
+            <div className={style.Top__titleBoxOnImage}>
+              <span className={style.Top__titleOnImage}>
+                LOOKBACK ON LASTYEAR
+              </span>
+              <span className={style.Top__titleOnImageSubTitle}>
+                + hello 2023, what i do?
+              </span>
+            </div>
           </div>
         </div>
+        <ScrollIndicator className={style.Top__scrollIndicator} />
       </div>
-      <ScrollIndicator className={style.Top__scrollIndicator} />
-    </div>
+    </>
   );
 };
 
